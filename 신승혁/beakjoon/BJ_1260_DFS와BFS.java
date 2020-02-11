72d623f7e3d7dc07fb0c384ee127d323a13ad787
SangJin Lim <diatkdwls@naver.com>
2020-02-11 14:20:53 +0900

    remove folder & rename file's name

---
+++ 신승혁/beakjoon/BJ_1260_DFS와BFS.java
@@ -0,0 +1,91 @@
+﻿package backjoon;
+
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+// 9시 10분 시작. 10시 6분 종료
+
+public class BJ_1260_DFS와BFS {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int N = Integer.parseInt(s[0]);
+        int M = Integer.parseInt(s[1]);
+        int V = Integer.parseInt(s[2]);
+
+        Point[] lines = new Point[M]; // 연결된 간선 담은 배열
+        boolean[] visitPoint = new boolean[N + 1]; // 방문 check 배열
+        Stack<Integer> stack = new Stack<>();
+        for (int m = 0; m < M; m++) {
+            String[] ss = br.readLine().split(" ");
+            lines[m] = new Point(Integer.parseInt(ss[0]), Integer.parseInt(ss[1]));
+        }
+
+        stack.push(V);
+        while (!stack.isEmpty()) {
+            int v = stack.pop();
+            // 방문했으면 다음 Point
+            if (visitPoint[v]) {
+                continue;
+            }
+            visitPoint[v] = true;
+            System.out.print(v + " ");
+            ArrayList<Integer> list = new ArrayList<>();
+            // 연결된점 찾아서 list에 담아서 오름차순
+            for (int i = 0; i < M; i++) {
+                if (lines[i].x == v) {
+                    list.add(lines[i].y);
+                } else if (lines[i].y == v) {
+                    list.add(lines[i].x);
+                }
+            }
+            Collections.sort(list, new SC());
+            // 연결된 점들 중에서 방문 안한 점만 push
+            for (int i : list) {
+                if (!visitPoint[i]) {
+                    stack.push(i);
+                }
+            }
+        }
+
+        System.out.println();
+        Arrays.fill(visitPoint, false);
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(V);
+        while (!queue.isEmpty()) {
+            int v = queue.poll();
+            if (visitPoint[v]) {
+                continue;
+            }
+            visitPoint[v] = true;
+            System.out.print(v + " ");
+            ArrayList<Integer> list = new ArrayList<>();
+            for (int i = 0; i < M; i++) {
+                if (lines[i].x == v) {
+                    list.add(lines[i].y);
+                } else if (lines[i].y == v) {
+                    list.add(lines[i].x);
+                }
+            }
+            Collections.sort(list);
+            for (int i : list) {
+                if (!visitPoint[i]) {
+                    queue.offer(i);
+                }
+            }
+        }
+
+
+    }
+
+    private static class SC implements Comparator<Integer> {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o2 - o1;
+        }
+    }
+}
---
+++ 신승혁/beakjoon/BJ_4485_녹색옷입은애가젤다지_2t.java
@@ -0,0 +1,80 @@
+﻿package beakjoon;
+
+import java.awt.Point;
+import java.io.*;
+import java.util.*;
+
+// 간다음에 확인하는게 아니라 가기전에 확인하고 들어가겠다.
+// Point 클래스 두 변수만 사용하기
+
+// pass
+
+public class BJ_4485_녹색옷입은애가젤다지_2t {
+	static int[][] map;
+	static int[][] valueMap;
+	static int[] dx = { 1, 0, -1, 0 };
+	static int[] dy = { 0, 1, 0, -1 };
+	static int N;
+	static int TC;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		TC = 1;
+		do {
+			N = Integer.parseInt(br.readLine());
+			if (N == 0) {
+				break;
+			}
+
+			map = new int[N][N];
+			valueMap = new int[N][N];
+			for (int i = 0; i < valueMap.length; i++) {
+				Arrays.fill(valueMap[i], -1);
+
+			}
+
+			for (int i = 0; i < N; i++) {
+				String[] s = br.readLine().split(" ");
+				for (int j = 0; j < s.length; j++) {
+					map[i][j] = Integer.parseInt(s[j]);
+				}
+			}
+			Queue<Point> queue = new LinkedList<>();
+//			int minValue = Integer.MAX_VALUE;
+			queue.offer(new Point(0, 0));
+			valueMap[0][0] = map[0][0];
+			while (!queue.isEmpty()) {
+				Point nowPoint = queue.poll();
+				int nowY = nowPoint.y;
+				int nowX = nowPoint.x;
+				int nowValue = valueMap[nowY][nowX];
+//				if (minValue < nowValue) {
+//					continue;
+//				}
+////				 minValue 했을 때 276ms 안했을때 364ms
+//
+//				if (nowX == N - 1 && nowY == N - 1) {
+//					if (minValue < nowValue) {
+//						minValue = nowValue;
+//					}
+//					continue;
+//				}
+				for (int d = 0; d < dx.length; d++) {
+					int nextX = nowX + dx[d];
+					int nextY = nowY + dy[d];
+					if (nextX >= 0 && nextY >= 0 && nextX < N && nextY < N
+							&& (valueMap[nextY][nextX] > nowValue + map[nextY][nextX]
+									|| valueMap[nextY][nextX] == -1)) {
+						valueMap[nextY][nextX] = nowValue + map[nextY][nextX];
+						queue.offer(new Point(nextX, nextY));
+					}
+				}
+			}
+			System.out.println("Problem " + (TC++) + ": " + valueMap[N-1][N-1]);
+		} while (true);
+
+		br.close();
+	}
+
+}
